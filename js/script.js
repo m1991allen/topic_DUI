a2b0b378fca7f93bf6d2dafb9753c5736938c4ae
m1991allen <48359142+m1991allen@users.noreply.github.com>
2019-06-14 10:44:18 +0800

    0614

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".various").fancybox({
     maxWidth: 800,
     maxHeight: 600,
@@ -11,25 +11,34 @@ $(document).ready(function() {
     closeEffect: "none"
   });
 
-  $("#hammer").mousedown(function() {
+  $("#hammer").mousedown(function () {
     $("#hammer>img").attr("src", "img/hammer_knock.png");
   });
-  $("#hammer").mouseup(function() {
+  $("#hammer").mouseup(function () {
     $("#hammer>img").attr("src", "img/hammer.png");
   });
 
-  $("#result").click(function() {
+  $("#result").click(function () {
     $("#info_answer").css("display", "block");
   });
-  $("#result1").click(function() {
+  $("#result1").click(function () {
     $("#info_answer1").css("display", "block");
   });
 
-  $(".fancybox-close").click(function() {
+  $(".fancybox-close").click(function () {
     $("#info_answer, #info_answer1").css("display", "none");
   });
 
-  // $(".car_img").click(function() {
-  //   $(".car_text").show(3000);
-  // });
+  $("#ask1").mouseover(function () {
+    $("#ask1_hover").css("display", "block");
+  });
+  $("#ask2").mouseover(function () {
+    $("#ask2_hover").css("display", "block");
+  });
+  $("#ask3").mouseover(function () {
+    $("#ask3_hover").css("display", "block");
+  });
+  $("#ask1,#ask2,#ask3").mouseleave(function () {
+    $("#ask1_hover,#ask2_hover,#ask3_hover").css("display", "none");
+  });
 });
