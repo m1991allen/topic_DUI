42fe4cd8672658d726a50a16b8f6fe4eda7e39eb
m1991allen <48359142+m1991allen@users.noreply.github.com>
2019-06-19 16:53:13 +0800

    fuck

--- js/script.js
+++ js/script.js
@@ -11,6 +11,13 @@ $(document).ready(function () {
     closeEffect: "none"
   });
 
+  function restTime(setting) {
+    myOption = setting;
+    document.getElementById("ans_sug").innerHTML = myOption;
+  }
+
+
+  // 錘子
   $("#hammer").mousedown(function () {
     $("#hammer>img").attr("src", "img/hammer_knock.png");
   });
@@ -18,6 +25,7 @@ $(document).ready(function () {
     $("#hammer>img").attr("src", "img/hammer.png");
   });
 
+  // 答案
   $("#result").click(function () {
     $("#info_answer").css("display", "block");
   });
@@ -25,10 +33,12 @@ $(document).ready(function () {
     $("#info_answer1").css("display", "block");
   });
 
+  // 燈箱關閉
   $(".fancybox-close").click(function () {
     $("#info_answer, #info_answer1").css("display", "none");
   });
 
+  // 人物訪談
   $("#ask1").mouseover(function () {
     $("#ask1_hover").css("display", "block");
   });
@@ -42,6 +52,7 @@ $(document).ready(function () {
     $("#ask1_hover,#ask2_hover,#ask3_hover").css("display", "none");
   });
 
+  // 酒精鎖地圖
   $("#set1").mouseover(function () {
     $("#lockMap>img").attr("src", "img/map1.png");
   });
@@ -51,9 +62,37 @@ $(document).ready(function () {
   $("#set3").mouseover(function () {
     $("#lockMap>img").attr("src", "img/map3.png");
   });
-});
 
-function restTime(setting) {
-  myOption = setting;
-  document.getElementById("ans_sug").innerHTML = myOption;
-}
+  // 酒精鎖切換
+
+  $("#lock").mouseover(function () {
+    $("#lock").attr("src", "img/lock1.png");
+  });
+  $("#lock").mouseleave(function () {
+    $("#lock").attr("src", "img/lock.png");
+  });
+
+  // 倒酒
+  let deg = 0;
+  $("#pour_bot").mousedown(function () {
+    if (deg < 7) {
+      $("#pour_deg").attr("src", "img/game/wine_" + (deg + 1) + ".svg");
+    }
+    deg++;
+  });
+  $("#pour_deg").mousedown(function () {
+    $("#pour_deg").attr("src", "img/game/wine_0.svg");
+    let deg_null = 0;
+    deg = deg_null;
+  });
+
+  // 翻牌
+  // let display = 
+  // $('.card').mousedown(function () {
+  //   $('.back').css('display', 'none');
+  // });
+
+  $('.card').click(function (event) {
+    $(event.currentTarget).toggleClass('flipped');
+  });
+})
