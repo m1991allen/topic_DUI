e99671293b83f59a027bff69987a110b4a24b53a
m1991allen <48359142+m1991allen@users.noreply.github.com>
2019-06-25 14:42:54 +0800

    0627

--- js/script.js
+++ js/script.js
@@ -103,13 +103,25 @@ $(document).ready(function () {
   });
 
   // 翻牌
-  // let display = 
-  // $('.card').mousedown(function () {
-  //   $('.back').css('display', 'none');
-  // });
-
   $('.card').click(function (event) {
     $(event.currentTarget).toggleClass('flipped');
   });
+
+  // gotop
+  $('#gotop').click(function (event) {
+    $('html, body').animate({
+      scrollTop: 0
+    }, '1000');
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 100) { //判斷捲軸小於200座標位置時，消失
+      $('#gotop').stop().fadeOut('fast');
+    } else {
+      $('#gotop').stop().fadeIn('fast'); //stop()可以讓使用者停止滾動時也跟著停止，不會持續做完
+      // $('#gotop').stop().fadeTo('fast', 1);
+      // fadeIn及fadeOut同時使用可能會有衝突，所以可以用fadeTo替換，但要加上透明度參數)
+    }
+  });
+
 
 })
