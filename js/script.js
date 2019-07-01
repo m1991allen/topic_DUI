45fe9549133e2ffd3e7a08851cfcf605723ec7f6
網新珍奶 <ftvrd02@wangxinzhennaide-MacBook-Pro.local>
2019-07-01 13:36:38 +0800

    0701

--- js/script.js
+++ js/script.js
@@ -22,10 +22,6 @@ $(document).ready(function () {
   });
 
   // 答案
-  // $("#result").click(function () {
-  //   $("#info_answer").css("display", "block");
-  //   $(".mask").css("display", "block")
-  // });
   $("#result1").click(function () {
     $("#info_answer1").css("display", "block");
     $(".mask").css("display", "block")
@@ -78,15 +74,6 @@ $(document).ready(function () {
     $("#lock").attr("src", "img/lock.png");
   });
 
-  // 倒酒
-  // let deg = 0;
-  // $("#pour_bot").mousedown(function () {
-  //   if (deg < 7) {
-  //     $("#pour_deg").attr("src", "img/game/wine_" + (deg + 1) + ".svg");
-  //   }
-  //   deg++;
-  //   
-  // });
 
   $("#pour_bot").mousedown(function () {
     $("#pour_deg").attr("src", "img/game/wine_7.svg");
@@ -104,20 +91,17 @@ $(document).ready(function () {
   });
 
   // gotop
-  $('#gotop').click(function (event) {
+  $('#gotop, #social').click(function (event) {
     $('html, body').animate({
       scrollTop: 0
     }, '1000');
   });
   $(window).scroll(function () {
     if ($(this).scrollTop() < 100) { //判斷捲軸小於200座標位置時，消失
-      $('#gotop').stop().fadeOut('fast');
+      $('#gotop, #social').stop().fadeOut('fast');
     } else {
-      $('#gotop').stop().fadeIn('fast'); //stop()可以讓使用者停止滾動時也跟著停止，不會持續做完
-      // $('#gotop').stop().fadeTo('fast', 1);
-      // fadeIn及fadeOut同時使用可能會有衝突，所以可以用fadeTo替換，但要加上透明度參數)
+      $('#gotop, #social').stop().fadeIn('fast'); //stop()可以讓使用者停止
     }
   });
-
 
 })
